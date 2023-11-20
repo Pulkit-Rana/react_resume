a2068016c5e336544100d8936ddfc4179be4bb0f
Pulkit-Rana <pulkit.rana221@gmail.com>
2023-11-21 03:35:44 +0530

    hero section added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,9 +2,20 @@ import React from 'react'
 import './Hero.css'
 
 export const Hero = () => {
+
     return (
-        <div >
-            <h1>About Me</h1>
+        <div className="hero">
+            <div className="hero-text">
+                <div className='i-name'></div>
+                <span>Hi! I Am</span>
+                <span>Pulkit Rana</span>
+                <span>I am a Java Developer</span>
+                <span>Hire me i am good motivated and have a lot of exp work with me hire me and get your mama to sit on my dick, get your sister to suck my cock</span>
+            </div>
+            <button className='button hero-button'>Hire Me</button>
+            <div className="hero-image">
+<h1>Hi I am right sode</h1>
+            </div>
         </div>
     )
 }
